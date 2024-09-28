f15a365f3c4b789c00db84b12fd3ae26a03c2df9
Tribuvana Kartikeya G <111010606+tribuvan@users.noreply.github.com>
2024-09-28 14:28:15 +0530

    Time: 25 ms (81.07%) | Memory: 11.6 MB (51.83%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def romanToInt(self, s):
+    
+        rti = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+        ans=0
+
+        for i in range(len(s)-1):
+            if rti[s[i]] < rti[s[i+1]]:
+                ans = ans - rti[s[i]]
+            else:   
+                ans = ans + rti[s[i]]
+
+        return ans+rti[s[-1]]
